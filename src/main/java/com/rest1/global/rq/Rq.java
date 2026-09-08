ec4a58571bbc99769488ccbca52867adadf94fab
chacha86 <taejincha0619@gmail.com>
2026-09-08 15:56:56 +0900

    init

---
+++ src/main/java/com/rest1/global/rq/Rq.java
@@ -0,0 +1,85 @@
+package com.rest1.global.rq;
+
+import com.rest1.domain.member.member.entity.Member;
+import com.rest1.domain.member.member.service.MemberService;
+import com.rest1.global.security.SecurityUser;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class Rq {
+
+    private final MemberService memberService;
+    private final HttpServletRequest request;
+    private final HttpServletResponse response;
+
+    public Member getActor() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        SecurityUser principal = (SecurityUser) authentication.getPrincipal();
+
+        long id = principal.getId();
+        String username = principal.getUsername();
+        String nickname = principal.getNickname();
+
+        Member member = new Member(id, username, nickname);
+
+        return member;
+    }
+
+    public void setHeader(String name, String value) {
+        response.setHeader(name, value);
+    }
+
+    public String getHeader(String name, String defaultValue) {
+        return Optional
+                .ofNullable(request.getHeader(name))
+                .filter(headerValue -> !headerValue.isBlank())
+                .orElse(defaultValue);
+    }
+
+    public String getCookieValue(String name, String defaultValue) {
+        return Optional
+                .ofNullable(request.getCookies())
+                .flatMap(
+                        cookies ->
+                                Arrays.stream(cookies)
+                                        .filter(cookie -> cookie.getName().equals(name))
+                                        .map(Cookie::getValue)
+                                        .filter(value -> !value.isBlank())
+                                        .findFirst()
+                )
+                .orElse(defaultValue);
+    }
+
+    public void setCookie(String name, String value) {
+        if (value == null) value = "";
+
+        Cookie cookie = new Cookie(name, value);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        cookie.setDomain("localhost");
+        cookie.setSecure(true);
+        cookie.setAttribute("SameSite", "Strict");
+
+        // 값이 없다면 해당 쿠키변수를 삭제하라는 뜻
+        if (value.isBlank()) {
+            cookie.setMaxAge(0);
+        }
+
+        response.addCookie(cookie);
+    }
+
+    public void deleteCookie(String name) {
+        setCookie(name, null);
+    }
+}
