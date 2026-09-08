9adf58f41d423e80a989c51f477aac238c70413d
chacha86 <taejincha0619@gmail.com>
2026-09-08 16:04:25 +0900

    3

--- src/main/java/com/rest1/global/security/SecurityBeanConfig.java
+++ src/main/java/com/rest1/global/security/SecurityBeanConfig.java
@@ -1,15 +1,36 @@
 package com.rest1.global.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class SecurityBeanConfig {
 
+    @Value("${custom.jwt.secretPattern}")
+    private String secretPattern;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    // 리소스 서버가 요구하는 단 하나의 부품 : "토큰 서명을 무엇으로 검증할 것인가"
+    // 우리는 우리가 발급한 JWT를 우리 secret으로 검증한다.
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        SecretKey secretKey = new SecretKeySpec(secretPattern.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+
+        return NimbusJwtDecoder
+                .withSecretKey(secretKey)
+                .build();
     }
 }
